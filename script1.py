7d6713e35fbc824e9d5c2f219c27b8ab3e5a0121
Pancu <pancu1222@gmail.com>
2021-07-01 00:06:01 +0200

    reading configuration and/or csv files

--- script1.py
+++ script1.py
@@ -1,7 +1,11 @@
-f = open("configuration.txt",'r')
-content = f.read()
-print(content)
-print(f.closed)
-f.close()
-print('closed')
-print(f.closed)
+
+import csv
+
+with open('configuration.txt') as file:
+    my_list = file.read().splitlines()
+print(my_list)
+
+with open('passwd.csv','r') as file:
+    reader = csv.reader(file, delimiter=':', lineterminator='\n')
+    for row in reader:
+        print(row)
