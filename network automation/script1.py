6bf43f7b3acc161593fca80a1097cf2ae54ae652
Pancu <pancu1222@gmail.com>
2021-07-02 01:21:40 +0200

    simple connecto to vm using paramiko and execute command

---
+++ network automation/script1.py
@@ -0,0 +1,11 @@
+import paramiko
+
+username = "spiderman"
+passwd="spiderman"
+ip='192.168.1.21'
+ssh_client=paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(hostname=ip,username=username,password=passwd)
+cmd="ls -ltr > test"
+
+stdin,stdout,stderr=ssh_client.exec_command(cmd)
