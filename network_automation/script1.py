cf18a2f408bfdb2f730a82fdad7dbc65eb58f0b1
Pancu <pancu1222@gmail.com>
2021-07-03 00:49:09 +0200

    automate process gathering from 2 servers

--- network_automation/script1.py
+++ network_automation/script1.py
@@ -2,23 +2,31 @@ import paramiko
 import time
 import json
 
-server={}
+server=[]
 with open('server.json','r') as f:
     server = json.load(f)
 
+def server_exec(server):
+    ssh_client=paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(**server,look_for_keys=False,allow_agent=False)
+    #shell=ssh_client.invoke_shell()
+    #shell.send('hostname\n')
+    #time.sleep(1)
+    #output=shell.recv(100000).decode('utf-8')
+    #print(output)
+    #cmd="ls -ltr > test"
 
-ssh_client=paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(**server,look_for_keys=False,allow_agent=False)
-shell=ssh_client.invoke_shell()
-shell.send('uname -a\n')
-time.sleep(1)
-output=shell.recv(100).decode('utf-8')
-print(output)
-#cmd="ls -ltr > test"
+    #stdin,stdout,stderr=ssh_client.exec_command(cmd)
+    command = 'ps aux | head\n'
+    (stdin, stdout, stderr) = ssh_client.exec_command(command)
 
-#stdin,stdout,stderr=ssh_client.exec_command(cmd)
+    cmd_output = stdout.read()
+    print(command, cmd_output.decode('utf-8'))
 
 
+    ssh_client.close()
 
-ssh_client.close()
+for idx,server in enumerate(server):
+    print('server number {}'.format(idx))
+    server_exec(server)
