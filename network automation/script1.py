8f5c9e15956b36be99070d8b0b44a7537a4bb2b8
Pancu <pancu1222@gmail.com>
2021-07-02 23:59:19 +0200

    server shell and output

--- network automation/script1.py
+++ network automation/script1.py
@@ -1,11 +1,25 @@
 import paramiko
+import time
+import json
 
-username = "spiderman"
-passwd="spiderman"
-ip='192.168.1.21'
+server={}
+with open('server.json','r') as f:
+    server = json.load(f)
+
+
+
 ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_client.connect(hostname=ip,username=username,password=passwd)
-cmd="ls -ltr > test"
+ssh_client.connect(**server,look_for_keys=False,allow_agent=False)
+shell=ssh_client.invoke_shell()
+shell.send('uname -a\n')
+time.sleep(1)
+output=shell.recv(100).decode('utf-8')
+print(output)
+#cmd="ls -ltr > test"
 
-stdin,stdout,stderr=ssh_client.exec_command(cmd)
+#stdin,stdout,stderr=ssh_client.exec_command(cmd)
+
+
+
+ssh_client.close()
